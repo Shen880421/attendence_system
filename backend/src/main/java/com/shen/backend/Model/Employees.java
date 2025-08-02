086637b71f4d5e771b0f2f3750800043cc2942af
Shen <solove5171@gmail.com>
2025-08-02 12:02:15 +0800

    feat(model): 新增員工模型的自動生成ID功能及格式化建構子

--- backend/src/main/java/com/shen/backend/Model/Employees.java
+++ backend/src/main/java/com/shen/backend/Model/Employees.java
@@ -4,13 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import java.util.Date;
 
 @Entity
 public class Employees {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String password;
@@ -20,7 +24,9 @@ public class Employees {
 
     private Date created_at;
 
-    public Employees() {}
+    public Employees() {
+    }
+
     public Employees(String name, String email, String password, Role role, Date created_at) {
         this.name = name;
         this.email = email;
@@ -31,6 +37,7 @@ public class Employees {
     public Long getId() {
         return id;
     }
+
     public void setMid(Long id) {
         this.id = id;
     }
@@ -38,6 +45,7 @@ public class Employees {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -45,15 +53,19 @@ public class Employees {
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public Date getCreateAt() {
         return created_at;
     }
